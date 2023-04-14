7cc55c7aba8888d62268e34b1bfeddb88c5dd3ad
EricR401S <eric.rios.3173@aol.com>
2023-04-14 18:43:17 -0400

    resize grayscale 1k script complete

--- resizeandextract-grayscale1K.py
+++ resizeandextract-grayscale1K.py
@@ -27,13 +27,13 @@ def extract_images_tar(tar_file, output_dir):
 # my code that got me the real images
 extract_images(
     r"C:\Users\ericr\Downloads\RealFaces00000.zip",
-    r"C:\Users\ericr\Desktop\IDS 705 - Machine Learning\projectv2repo\IDS705_ML_Team9\TemporaryFiles\Real",
+    ".\TemporaryFiles\Real",
 )
 
 # my code that got me the fake images
 extract_images_tar(
     r"C:\Users\ericr\Downloads\1m_faces_00.tar",
-    r"C:\Users\ericr\Desktop\IDS 705 - Machine Learning\projectv2repo\IDS705_ML_Team9\TemporaryFiles\Fake",
+    ".\TemporaryFiles\Fake",
 )
 
 # def resize_images(input_dir, output_dir, size):
@@ -44,9 +44,9 @@ extract_images_tar(
 #         cv2.imwrite(os.path.join(output_dir, os.path.basename(image)), img)
 
 # erase image files in output directory
-def erase_images(output_dir):
+def erase_images(output_dir, format):
     """Erase images in output directory"""
-    for image in glob.glob(os.path.join(output_dir, "*.jpg")):
+    for image in glob.glob(os.path.join(output_dir, "*." + format)):
         os.remove(image)
 
 
@@ -74,6 +74,8 @@ def resize_and_erase_images(
         img = Image.open(image)
         try:
             img = img.resize(size)
+            # grayscale
+            img = img.convert("L")
 
         except:
             print(f"Image at {iter+1} is not a valid image")
@@ -92,19 +94,22 @@ def resize_and_erase_images(
 size = (256, 256)
 # has a scalability advantage if the directory structure does not vary, can be for looped
 resize_and_erase_images(
-    r"TemporaryFiles\Real\00000",
-    "One1ksetDraft\Real",
+    ".\TemporaryFiles\Real\\00000",
+    ".\grayscale1kdraft\Real",
     size,
     "png",
     flag=True,
 )
 resize_and_erase_images(
-    r"TemporaryFiles\Fake\1m_faces_00",
-    "One1ksetDraft\Fake",
+    ".\TemporaryFiles\Fake\\1m_faces_00",
+    ".\grayscale1kdraft\Fake",
     size,
     "jpg",
     flag=False,
 )
+
+# cleaning the 1m_faces_00 directory
+erase_images(".\TemporaryFiles\Fake\\1m_faces_00", "jpg")
 
 
 # def main():
