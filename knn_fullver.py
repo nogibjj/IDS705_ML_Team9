4c0e06e0341723292833f8320c054b8623ae9fd0
Kashaf Ali <111402572+kashafali8@users.noreply.github.com>
2023-04-16 05:12:58 +0000

    knn

--- knn_fullver.py
+++ knn_fullver.py
@@ -1,14 +1,75 @@
 import pandas as pd
 import numpy as np
-
+import warnings
+warnings.filterwarnings("ignore")
 from sklearn.neighbors import KNeighborsClassifier
 
 # Load training features and labels
-train_features = np.load('train_features.npy')
-train_labels = np.load('train_labels.npy')
+train_features = np.load(
+    "/Users/kashafali/Documents/Duke/Spring23/ML/Project/20karrrays/20Ktrain_images.npy"
+)
+train_labels = np.load(
+    "/Users/kashafali/Documents/Duke/Spring23/ML/Project/20karrrays/20Ktrain_labels.npy"
+)
 
+num_samples = train_features.shape[0]
+num_features = np.prod(train_features.shape[1:])
+train_features_2d = np.reshape(train_features, (num_samples, num_features))
 
-# Train KNN model
-k = 5  # number of neighbors to consider
-knn_model = KNeighborsClassifier(n_neighbors=k)
-knn_model.fit(train_features, train_labels)
+# Load validation features and labels
+val_features = np.load(
+    "/Users/kashafali/Documents/Duke/Spring23/ML/Project/20karrrays/20Kval_images.npy"
+)
+val_labels = np.load(
+    "/Users/kashafali/Documents/Duke/Spring23/ML/Project/20karrrays/20Kval_labels.npy"
+)
+
+num_samples = val_features.shape[0]
+num_features = np.prod(val_features.shape[1:])
+val_features_2d = np.reshape(val_features, (num_samples, num_features))
+
+# Load test features and labels
+test_features = np.load(
+    "/Users/kashafali/Documents/Duke/Spring23/ML/Project/20karrrays/20Ktest_images.npy"
+)
+test_labels = np.load(
+    "/Users/kashafali/Documents/Duke/Spring23/ML/Project/20karrrays/20Ktest_labels.npy"
+)
+
+num_samples = test_features.shape[0]
+num_features = np.prod(test_features.shape[1:])
+test_features_2d = np.reshape(test_features, (num_samples, num_features))
+
+# Create a KNN classifier
+knn = KNeighborsClassifier(n_neighbors=3)
+
+# Fit the classifier to the training data
+knn.fit(train_features_2d, train_labels)
+
+# Make predictions on the validation data
+val_predictions = knn.predict(val_features_2d)
+
+# Calculate the accuracy of the predictions
+accuracy = np.mean(val_predictions == val_labels)
+print("Validation Accuracy: ", accuracy)
+
+# Create ROC curve
+from sklearn.metrics import roc_curve, auc
+import matplotlib.pyplot as plt
+
+fpr, tpr, thresholds = roc_curve(val_labels, val_predictions)
+roc_auc = auc(fpr, tpr)
+
+plt.figure()
+lw = 2
+plt.plot(
+    fpr, tpr, color="darkorange", lw=lw, label="ROC curve (area = %0.2f)" % roc_auc
+)
+plt.plot([0, 1], [0, 1], color="navy", lw=lw, linestyle="--")
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel("False Positive Rate")
+plt.ylabel("True Positive Rate")
+plt.title("Receiver operating characteristic")
+plt.legend(loc="lower right")
+plt.show()
