bd524342146a1e247e1fe207c2d82ca96173926e
Beibei(Bella) Du <60382493+belladu0201@users.noreply.github.com>
2023-04-17 03:47:48 +0000

    change

--- plot.py
+++ plot.py
@@ -7,7 +7,6 @@ def roc_curve(labels_ls, test_ds_pred):
     test_ds_pred_other_class = 1 - test_ds_pred
     test_ds_pred_rounded = np.round(test_ds_pred_other_class)
     np.sum(test_ds_pred_rounded == labels_ls) / len(labels_ls)
-    # roc curve for models
     fpr, tpr, thresh = roc_curve(labels_ls, test_ds_pred)
     # plot roc curve
     plotter_lib.plot(fpr, tpr, linestyle="--", color="orange", label="ResNet50")
@@ -20,14 +19,24 @@ def roc_curve(labels_ls, test_ds_pred):
 
 def pr_curve(labels_ls, test_ds_pred):
     # calculate the no skill line as the proportion of the positive class
-    no_skill = len(labels_ls[labels_ls==1]) / len(labels_ls)
+    fake = len(labels_ls[labels_ls==1]) / len(labels_ls)
     # calculate model precision-recall curve
     precision, recall, _ = precision_recall_curve(labels_ls, test_ds_pred)
     # plot the precision-recall curves
-    plotter_lib.plot([0, 1], [no_skill, no_skill], linestyle='--', label='No Skill')
+    plotter_lib.plot([0, 1], [fake, fake], linestyle='--', label='No Skill')
     plotter_lib.plot(recall, precision, marker='.', label='ResNet50')
     # axis labels
     plotter_lib.xlabel('Recall')
     plotter_lib.ylabel('Precision')
     plotter_lib.legend()
     plotter_lib.show()
+
+# generate fake data
+labels_ls = np.random.randint(0, 2, 100)
+test_ds_pred = np.random.rand(100)
+
+# plot roc curve
+roc_curve(labels_ls, test_ds_pred)
+
+# plot pr curve
+pr_curve(labels_ls, test_ds_pred)
